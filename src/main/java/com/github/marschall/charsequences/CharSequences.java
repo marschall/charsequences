69655f7eb1b66e04c5a0f4baa0d4adbad71e843a
Philippe Marschall <philippe.marschall@gmail.com>
2016-12-29 17:57:51 +0100

    tests for overflow and underflow

--- src/main/java/com/github/marschall/charsequences/CharSequences.java
+++ src/main/java/com/github/marschall/charsequences/CharSequences.java
@@ -3,10 +3,65 @@ package com.github.marschall.charsequences;
 /**
  * Utility methods for dealing with {@link CharSequence} objects.
  */
-public class CharSequences {
+public final class CharSequences {
 
   private CharSequences() {
     throw new AssertionError("not instantiable");
+  }
+
+  public static int parseInt(CharSequence charSequence) {
+    if (charSequence == null) {
+      throw new NumberFormatException("null");
+    }
+    int length = charSequence.length();
+    if (length == 0) {
+      throw invalidDecimalNumber(charSequence);
+    }
+    char first = charSequence.charAt(0);
+    int start;
+    boolean negative;
+    if (first == '-') {
+      negative = true;
+      start = 1;
+    } else if (first == '+') {
+      negative = false;
+      start = 1;
+    } else {
+      negative = false;
+      start = 0;
+    }
+
+    if (length - start == 0) {
+      throw invalidDecimalNumber(charSequence);
+    }
+
+    int product = 0;
+    // Integer.MIN_VALUE does not have a positive representation but
+    // Integer.MAX_VALUE has a negative representation so build negative numbers
+    for (int i = start; i < length; ++i) {
+      char c = charSequence.charAt(i);
+      if (c < '0' || c > '9') {
+        throw invalidDecimalNumber(charSequence);
+      }
+      product = product * 10 - (c - '0');
+    }
+
+    if (negative) {
+      return product;
+    } else {
+      return -product;
+    }
+  }
+
+  public static long parseLong(CharSequence charSequence) {
+    if (charSequence == null) {
+      throw new NumberFormatException("null");
+    }
+    return 0;
+  }
+
+  private static NumberFormatException invalidDecimalNumber(CharSequence charSequence) {
+    return new NumberFormatException("invalid decimal number " + charSequence);
   }
 
   public static CharSequence betweenAnd(CharSequence charSequence, char start, char end) {
--- src/test/java/com/github/marschall/charsequences/CharSequencesTest.java
+++ src/test/java/com/github/marschall/charsequences/CharSequencesTest.java
@@ -2,10 +2,92 @@ package com.github.marschall.charsequences;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class CharSequencesTest {
+
+  @Test
+  public void validInts() {
+    assertParseInt(0, "+0");
+    assertParseInt(0, "-0");
+    assertParseInt(0, "0");
+
+    assertParseInt(0, "00");
+    assertParseInt(1, "01");
+    // more 0 than Integer.MAX_VALUE has places
+    assertParseInt(1, "000000000001");
+    assertParseInt(0, "+00");
+    assertParseInt(1, "+01");
+    assertParseInt(0, "-00");
+    assertParseInt(-1, "-01");
+
+    assertParseInt(1, "1");
+    assertParseInt(-1, "-1");
+    assertParseInt(Integer.MAX_VALUE, Integer.toString(Integer.MAX_VALUE));
+    assertParseInt(Integer.MIN_VALUE, Integer.toString(Integer.MIN_VALUE));
+    assertParseInt(Integer.MAX_VALUE, "00" + Integer.toString(Integer.MAX_VALUE));
+    assertParseInt(Integer.MIN_VALUE, Integer.toString(Integer.MIN_VALUE));
+  }
+
+  @Test
+  public void invalidInts() {
+    assertInvalidInt(null);
+    assertInvalidInt("");
+    assertInvalidInt("+");
+    assertInvalidInt("-");
+    assertInvalidInt("++0");
+    assertInvalidInt("+-0");
+    assertInvalidInt("-+0");
+    assertInvalidInt("--0");
+
+    assertInvalidInt("0 ");
+    assertInvalidInt(" 0");
+    assertInvalidInt("0a");
+    assertInvalidInt("a0");
+    assertInvalidInt("0/");
+    assertInvalidInt("/0");
+    assertInvalidInt("0:");
+    assertInvalidInt(":0");
+
+    assertInvalidInt(Long.toString((long) Integer.MAX_VALUE + 1L));
+    assertInvalidInt("1" + Long.toString((long) Integer.MAX_VALUE + 1L));
+    assertInvalidInt("-12" + Long.toString((long) Integer.MIN_VALUE - 1L).substring(1));
+  }
+
+  private void assertParseInt(int expected, CharSequence charSequence) {
+    assertEquals(expected, CharSequences.parseInt(charSequence));
+    assertEquals(expected, Integer.parseInt(charSequence.toString()));
+  }
+
+  private void assertInvalidInt(CharSequence charSequence) {
+    try {
+      CharSequences.parseInt(charSequence);
+      fail("should be invalid \"" + charSequence + "\"");
+    } catch (NumberFormatException e) {
+      if (charSequence != null) {
+        assertTrue(e.getMessage().contains(charSequence));
+      } else {
+        assertTrue(e.getMessage().contains("null"));
+      }
+    }
+    try {
+      if (charSequence != null) {
+        Integer.parseInt(charSequence.toString());
+      } else {
+        Integer.parseInt(null);
+      }
+      fail("should be invalid \"" + charSequence + "\"");
+    } catch (NumberFormatException e) {
+      if (charSequence != null) {
+        assertTrue(e.getMessage().contains(charSequence));
+      } else {
+        assertTrue(e.getMessage().contains("null"));
+      }
+    }
+  }
 
   @Test
   public void indexOf() {
